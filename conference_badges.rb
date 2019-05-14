0f7b99d86a9bfe973fba6cc1d7d3b76b14457e0a
Dennis <dennis.basyrov@gmail.com>
2019-05-14 03:05:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end  
 
-def batch_badge_creator
-  
-end   
+def batch_badge_creator(array)
+  new_array = []
+  array.each do 
+    new_array << "Hello, my name is #{i}."
+end
+end
